95fb66ba477a533d4fc036902fa8b97b888eaf98
万容嘉 <wanrongjia@lingyangtech.com>
2023-03-13 11:50:24 +0800

    build

--- vite.config.js
+++ vite.config.js
@@ -39,6 +39,19 @@ export default ({ mode }) => {
 		},
 		build: {
 			outDir: 'docs', // 指定输出路径
+			chunkSizeWarningLimit: 1500,
+			rollupOptions: {
+				output: {
+					chunkFileNames: 'static/js/[name]-[hash].js',
+					entryFileNames: 'static/js/[name]-[hash].js',
+					assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+				}
+			},
+			manualChunks(id) {
+				if (id.includes('node_modules')) {
+					return id.toString().split('node_modules/')[1].split('/')[0].toString();
+				}
+			},
 		},
 	})
 }
